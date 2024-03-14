dd9911c5b8e479ebd4c9233d2c7366ee636aaa8b
TapiwaHerbetNosenga <144127869+TapiwaHerbetNosenga@users.noreply.github.com>
2024-03-14 14:01:31 +0200

    initial commit

--- script.js
+++ script.js
@@ -1,12 +1,37 @@
 function validateSyntax() {
     let input = document.getElementById('petInput').value;
-    // Validation logic goes here
-    let result = ''; // Placeholder for validation result
+    let result = "Valid Syntax";
+    
+if (input.startsWith("pet_" ) && input.includes(" ") !== true){
 
+    if(!isNaN(input[4]) && !isNaN(input[5]) && !isNaN(input[6]) && !isNaN(input[7])){
+        let i = 8;
+        while(i<input.length){
+
+            if( /^[a-zA-Z]+$/.test(input[i]) === false){
+                 result = "Invalid Syntax";
+                 break;
+            }
+            else{
+                i++;
+            }
+        }    
+    }
+    else{
+        result = "Invalid Syntax";
+    }
+    
+}
+else{
+    result = "Invalid Syntax";
+}
     // TODO: Write your validation logic here
         // Check if input starts with 'pet_' and followed by alphanumeric characters
 
             document.getElementById('result').innerText = result;
 }
+
+
+
 
 
